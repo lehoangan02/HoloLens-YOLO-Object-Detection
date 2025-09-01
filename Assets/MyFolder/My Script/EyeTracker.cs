fb92da05a0f82f8bdda66cd18def5b087c0ee0b1
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2025-09-01 21:47:18 +0700

    Add EyeTracker script and update MRTK to 4.x
    Added EyeTracker.cs script and related prefab, material, and scene objects for gaze tracking functionality. Upgraded Mixed Reality Toolkit (MRTK) packages from 3.x to 4.0.0-pre.1 and added the accessibility package. Updated the main scene to include new objects and EyeTracker component, and adjusted package manifest and lock files accordingly.

---
+++ Assets/MyFolder/My Script/EyeTracker.cs
@@ -0,0 +1,56 @@
+using MixedReality.Toolkit.Input;
+using System;
+using System.IO;
+using UnityEngine;
+
+public class EyeTracker : MonoBehaviour
+{
+    public GazeInteractor gazeInteractor;         // From MRTK3
+    public GameObject objectOfInterest;           // The target object
+    public GameObject hitPointDisplayPrefab;      // Prefab to show gaze hit point
+
+    private GameObject hitPointDisplayer;
+    private StreamWriter trackerData;
+
+    private void Awake()
+    {
+        var trackerDataPath = Path.Combine(Application.persistentDataPath,
+                                           "eyetracking.csv");
+        trackerData = new StreamWriter(trackerDataPath);
+        trackerData.AutoFlush = true;
+    }
+
+    private void Start()
+    {
+        hitPointDisplayer = Instantiate(hitPointDisplayPrefab);
+    }
+
+    private void Update()
+    {
+        if (gazeInteractor == null || objectOfInterest == null) return;
+
+        var ray = new Ray(gazeInteractor.rayOriginTransform.position,
+                          gazeInteractor.rayOriginTransform.forward * 3);
+
+        if (Physics.Raycast(ray, out var hit))
+        {
+            if (hit.collider.gameObject == objectOfInterest)
+            {
+                hitPointDisplayer.transform.position = hit.point;
+                WriteTrackingPoint(hit.point);
+            }
+        }
+    }
+
+    private void WriteTrackingPoint(Vector3 hitPoint)
+    {
+        var relativePoint = objectOfInterest.transform.position - hitPoint;
+        trackerData.WriteLine(FormattableString.Invariant(
+            $"{relativePoint.x},{relativePoint.y},{relativePoint.z}"));
+    }
+
+    private void OnDestroy()
+    {
+        trackerData.Close();
+    }
+}
